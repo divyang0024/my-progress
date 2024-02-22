9f67dfd821e4ede16c7c71d9478fc65696bcb724
Divyang <divyang0024@gmail.com>
2024-02-22 11:38:15 +0530

    javascript for of and in

---
+++ javascript/detour/17forof&in.js
@@ -0,0 +1,23 @@
+// 'for of' is used in the case of array and string but it cant be used for objects.
+// 'for in' is used for objects and it gives key of object and index of array and string.
+
+let arr = [12, 23, 34, 45, 56, 67];
+let str = "this is a string";
+let object = { name: "divyang", sex: "male" };
+
+for (let index of arr) {
+  console.log(index); //directll returns a values.
+}
+for (let index of str) {
+  console.log(index);
+}
+// for (let index of object) {
+//   console.log(index);
+// }can not work for objects
+
+for (let index in str) {
+  console.log(index);
+}
+for (let key in object) {
+  console.log(key);
+}
